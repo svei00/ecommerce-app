7015f39b90d49e4c299ee94e169c2d3fee5448ca
svei00 <79055634+svei00@users.noreply.github.com>
2022-10-08 23:07:58 -0700

    Create Hero Component and some minor components

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from 'react';
 
-const index = () => {
+import { client } from '../lib/client';
+import { Hero, FooterBanner, Product } from '../components';
+
+const Home = ( { products, bannerData } ) => {
   return (
-    <div>index</div>
-  )
+    <>
+      <Hero heroBanner = {bannerData.length && bannerData[0]} />
+      {console.log(bannerData)}
+      <div className='products-heading'>
+        <h2>Best Selling Products</h2>
+        <p>Croquetas Dog Chow</p>
+      </div>
+
+      <div className='products-container'>
+        {
+          products?.map((product) => product.name)  // The question mark is to ensure we have data
+        }
+      </div>
+      
+      <FooterBanner />
+    </>
+  );
 }
 
-export default index
+export const getServerSideProps = async() => {
+  // Getting the query
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  // Banner
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData }
+  }
+}
+
+export default Home;
