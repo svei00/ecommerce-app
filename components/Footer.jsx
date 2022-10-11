66df2bb4248b3db5a36082ea80809ed2de5b5e4e
svei00 <79055634+svei00@users.noreply.github.com>
2022-10-10 19:10:08 -0700

    Added the layout plus header and footer in the layout

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { AiFillInstagram, AiOutlineTwitter, AiFillFacebook } from 'react-icons/ai';
+import { getCurrentYear } from '../assets/js/scripts';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer-container'>
+      <p>
+        2020 - {getCurrentYear()} Croquetas "El Kilo". All Rights Reserverd&reg;
+      </p>
+      <p className='icons'>
+        <AiFillInstagram />
+        <AiFillFacebook />
+        <AiOutlineTwitter />
+      </p>
+    </div>
   )
 }
 
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import Head from 'next/head';
+import Navbar from './Navbar';
+import Footer from './Footer';
 
-const Layout = () => {
+const Layout = ( {children} ) => {          // Remember that in React we pass the props throught children
   return (
-    <div>Layout</div>
+    <div className='layout'>
+      <Head>
+        <title>Croquetas "El Kilo"</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className='main-container'>
+        {children}                        
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
   )
 }
 
